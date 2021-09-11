4d723df42032732a469d426310746a5715108039
Jesse De Jong <jdoceau@gmail.com>
2021-09-11 12:50:45 +1000

    blocked out directory structure, imported server.js from previous project

---
+++ server.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./routes');
+// const helpers = require('./utils/helpers');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+// import sequelize connection
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Setup sessions
+const sess = {
+  secret: 'keyboard cat',
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+  resave: false,
+  saveUninitialized: true,
+  // cookie: { secure: true }
+};
+
+app.use(session(sess));
+
+// Setup handlebars
+// const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
